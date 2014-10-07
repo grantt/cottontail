95e4a70823ad5f3ab4e9e9bd05b331cc8a6e92d3
Grant Toeppen <grant@adroll.com>
2014-10-07 00:00:19 -0700

    Topic example

---
+++ cottontail/examples/topic/publisher.py
@@ -0,0 +1,15 @@
+import cottontail
+
+
+def run():
+    test_cli = cottontail.CottontailClient('test_topic', exchange_type=cottontail.EXCHANGE_TOPIC)
+
+    for n in xrange(100):
+        if n % 2:
+            topic = 'something.test'
+        else:
+            topic = 'something.other'
+        test_cli.publish(topic, str(n))
+
+if __name__ == '__main__':
+    run()
