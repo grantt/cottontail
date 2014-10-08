03e295b7f5b20c1368d5900f7de6763d33fca3b3
Grant Toeppen <grant@adroll.com>
2014-10-08 15:44:24 -0700

    mock out pyev

--- docs/conf.py
+++ docs/conf.py
@@ -276,5 +276,5 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
         return Mock()
 
-MOCK_MODULES = ['libev']
+MOCK_MODULES = ['pyev']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
